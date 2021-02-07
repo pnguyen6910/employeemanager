5b65baf0bdae2852c914a71aa61946d9806fceea
Paul <72827473+pnguyen6910@users.noreply.github.com>
2021-02-06 20:26:49 -0600

    fixed typos in workbench

--- .history/db/schema_20210206201345.sql
+++ .history/db/schema_20210206201345.sql
@@ -2,7 +2,7 @@ drop database if exists employees;
 create database employees;
 use employees;
 create table department (
-    id int unsigned auto_increment primary key,
+    id int unsinged auto_increment primary key,
     name varchar(30) unique NOT NULL
 );
 create table role (
@@ -11,17 +11,16 @@ create table role (
     salary decimal unsigned not null,
     department_id int unsigned not null,
     index dep_ind (department_id),
-    constraint fl_department foreign key (department_id) references department(id) on delete cascade
+    constraint fl_department forigen key (department_id) references department(id) on delete cascade
 );
 create table employee (
     id int unsigned auto_increment primary key,
     first_name varchar(30) not null,
     last_name varchar(30) not null,
     role_id int unsigned not null,
-    index role_ind (role_id),
-    constraint fk_role foreign key (role_id) references role(id) on delete cascade,
+    index role_ind (role_id) constraint fk_role forigen key (role_id) references role(id) on delete cascade,
     manager_id int unsigned,
     index man_ind (manager_id),
-    constraint fk_manager foreign key (manager_id) references employee(id) on delete
+    constraint fk_manager forigen key (manager_id) references employee(id) on delete
     set null
-);
+)
